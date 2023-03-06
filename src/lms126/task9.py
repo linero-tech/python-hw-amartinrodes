98133066babc5533ecd8a1eb18a1c1ef2608b6ae
amartinrodes <amartinrodes@gmail.com>
2023-03-06 17:57:52 +0100

    Update task9.py

--- src/lms126/task9.py
+++ src/lms126/task9.py
@@ -2,4 +2,17 @@ from to_do import TODO
 
 
 def task9():
-    return TODO("Erase this line and write your solution")
+    return """
+    insert card
+    INPUT pin
+    IF wrong
+    OUTPUT error
+    ELSE chose withdraw
+    INPUT amount to withdraw
+    IF amount<balance
+    OUTPUT error
+    ELSE give money
+    update balance
+    return the card
+    
+    """

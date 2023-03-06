c6679d46dee13657cbb0103d4e01c82699b15f61
amartinrodes <amartinrodes@gmail.com>
2023-03-06 18:46:34 +0100

    Update task11.py

--- src/lms126/task11.py
+++ src/lms126/task11.py
@@ -2,4 +2,17 @@ from to_do import TODO
 
 
 def task11():
-    return TODO("Erase this line and write your solution")
+    return """
+    INPUT temperature
+    break number and character
+    IF character equals C
+    apply the C-F formula to number
+    OUTPUT result from formula and switch original character
+    IF character equals F
+    apply the F-C formula to number
+    OUTPUT result from formula and switch original character
+    ELSE OUTPUT error
+    """
+
+
+
